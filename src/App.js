2e6af5bcd17da4bb847608817a607a347a925ff7
Rabyn900 <82014570+Rabyn-Collab@users.noreply.github.com>
2024-01-14 09:30:16 +0545

    ch

--- src/App.js
+++ src/App.js
@@ -1,19 +1,29 @@
 import React from 'react'
-import { meals } from './dummy/meals'
+
 
 const App = () => {
 
 
-  return (
-    <div className='p-4 grid grid-cols-3 gap-5 '>
 
-      {meals.map((meal, i) => {
-        return <div className='shadow-xl p-2' key={meal.idCategory}>
-          <h1>{meal.strCategory}</h1>
-          <img src={meal.strCategoryThumb} alt="" />
-          <p>{meal.strCategoryDescription.substring(0, 100) + '....'}</p>
-        </div>
-      })}
+  const reactangle = {
+    length: 20,
+    width: 200,
+    totalArea: function () {
+      return this.length * this.width;
+    },
+  };
+
+  const totals = reactangle.totalArea();
+
+
+  console.log(totals);
+
+
+  return (
+    <div>
+
+
+
 
     </div>
   )
