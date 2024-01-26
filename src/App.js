7c532d9c77c987c8f87b3827e431e19b700c030d
Rabyn900 <82014570+Rabyn-Collab@users.noreply.github.com>
2024-01-26 08:18:03 +0545

    ch

--- src/App.js
+++ src/App.js
@@ -1,26 +1,8 @@
 import React from 'react'
-import Header from './components/Header'
-import DisplaySecion from './pages/DisplaySecion'
-import Technologies from './pages/Technologies'
-import Intro from './pages/Intro'
-import Footer from './components/Footer'
 
 const App = () => {
-
-
   return (
-    <div>
-
-      <Header />
-      <DisplaySecion />
-      <Technologies />
-      <Intro />
-      <Footer />
-
-
-
-
-    </div>
+    <div>App</div>
   )
 }
 
