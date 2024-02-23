4386d6599431749a896fda8d3aef5bc0d9f400b2
Rabyn900 <82014570+Rabyn-Collab@users.noreply.github.com>
2024-02-23 08:31:18 +0545

    ch

---
+++ src/dummy/artistsData.js
@@ -0,0 +1,101 @@
+
+
+
+export const artists = {
+  "status": true,
+  "result": {
+    "artists": {
+      "hits": [
+        {
+          "artist": {
+            "avatar": "https://is1-ssl.mzstatic.com/image/thumb/Music112/v4/26/53/90/265390af-e4b3-22d6-20cc-a8ad19551d01/pr_source.png/800x800bb.jpg",
+            "name": "Shakira",
+            "verified": false,
+            "weburl": "https://music.apple.com/gb/artist/shakira/889327",
+            "adamid": "889327"
+          }
+        },
+        {
+          "artist": {
+            "name": "Shakira Ramirez",
+            "verified": false,
+            "weburl": "https://music.apple.com/gb/artist/shakira-ramirez/377696677",
+            "adamid": "377696677"
+          }
+        },
+        {
+          "artist": {
+            "avatar": "https://is1-ssl.mzstatic.com/image/thumb/Music116/v4/41/66/fb/4166fb41-e8de-c03a-290c-1eb564a33794/23UMGIM25567.rgb.jpg/800x800ac.jpg",
+            "name": "Shakira Martínez",
+            "verified": false,
+            "weburl": "https://music.apple.com/gb/artist/shakira-mart%C3%ADnez/1410317660",
+            "adamid": "1410317660"
+          }
+        },
+        {
+          "artist": {
+            "name": "Shakira Jones",
+            "verified": false,
+            "weburl": "https://music.apple.com/gb/artist/shakira-jones/347778251",
+            "adamid": "347778251"
+          }
+        },
+        {
+          "artist": {
+            "avatar": "https://is1-ssl.mzstatic.com/image/thumb/Features113/v4/37/f5/8e/37f58ea9-33cf-fa71-564e-f33814ee6db2/mzl.dkpqybxd.png/800x800bb.jpg",
+            "name": "Shakira Jasmine",
+            "verified": false,
+            "weburl": "https://music.apple.com/gb/artist/shakira-jasmine/1345928293",
+            "adamid": "1345928293"
+          }
+        },
+        {
+          "artist": {
+            "avatar": "https://is1-ssl.mzstatic.com/image/thumb/Music124/v4/9e/96/05/9e96053c-7927-df34-24b0-d93ebad64f41/mzi.jhkzloqz.jpg/800x800ac.jpg",
+            "name": "House of Shakira",
+            "verified": false,
+            "weburl": "https://music.apple.com/gb/artist/house-of-shakira/128801449",
+            "adamid": "128801449"
+          }
+        },
+        {
+          "artist": {
+            "avatar": "https://is1-ssl.mzstatic.com/image/thumb/Music113/v4/f2/b4/71/f2b471ce-6f8f-65c5-8285-966355153c61/pr_source.png/800x800bb.jpg",
+            "name": "Shakira Aly",
+            "verified": false,
+            "weburl": "https://music.apple.com/gb/artist/shakira-aly/1384070523",
+            "adamid": "1384070523"
+          }
+        },
+        {
+          "artist": {
+            "avatar": "https://is1-ssl.mzstatic.com/image/thumb/Music115/v4/b6/01/ca/b601ca95-445b-406b-2745-e3c6f51e9b6d/pr_source.png/800x800bb.jpg",
+            "name": "Shakira Shakiraa",
+            "verified": false,
+            "weburl": "https://music.apple.com/gb/artist/shakira-shakiraa/1559258318",
+            "adamid": "1559258318"
+          }
+        },
+        {
+          "artist": {
+            "avatar": "https://is1-ssl.mzstatic.com/image/thumb/Music113/v4/f7/a9/1a/f7a91ae5-ef32-b8df-aa79-3722ba074928/pr_source.png/800x800bb.jpg",
+            "name": "Shakira Alleyne",
+            "verified": false,
+            "weburl": "https://music.apple.com/gb/artist/shakira-alleyne/1317066096",
+            "adamid": "1317066096"
+          }
+        },
+        {
+          "artist": {
+            "avatar": "https://is1-ssl.mzstatic.com/image/thumb/Music126/v4/c3/25/f7/c325f79b-09d4-8a16-0e76-db277b643d8f/0.jpg/800x800ac.jpg",
+            "name": "Shakira shakiraa",
+            "verified": false,
+            "weburl": "https://music.apple.com/gb/artist/shakira-shakiraa/1619680416",
+            "adamid": "1619680416"
+          }
+        }
+      ],
+      "next": "https://cdn.shazam.com/search/v4/en-US/GB/web/search?term=shakira&offset=10&limit=10&types=artists"
+    }
+  }
+};
--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,22 +1,56 @@
+import axios from 'axios'
 import React from 'react'
-import CardDisplay from '../components/CardDisplay'
-import UserForm from '../components/UserForm';
+import { useEffect } from 'react';
+import { useState } from 'react';
 
 const HomePage = () => {
 
-  const image = 'https://images.unsplash.com/photo-1706391989349-7882ece26c0c?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHw3fHx8ZW58MHx8fHx8';
-  const image1 = 'https://images.unsplash.com/photo-1682686581854-5e71f58e7e3f?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDF8MHxlZGl0b3JpYWwtZmVlZHw2fHx8ZW58MHx8fHx8';
+  const [data, setData] = useState({});
+
+
+  const fetchData = async () => {
+    try {
+      const response = await axios.get('', {
+        params: {
+          api_key: ''
+        }
+      });
+
+    } catch (err) {
+
+    }
+  }
+
+  console.log('heloo jeee');
+
+  // console.log('run1');
+  // console.log('run2');
+  // setTimeout(() => {
+  //   console.log('hello jee run4');
+  // }, 4000);
+  // console.log('run3')
+
+  // axios.get('https://api.themoviedb.org/3/movie/popular',
+  //   {
+  //     params: {
+  //       'api_key': '92c1e33f015755d27a231793c44ecfed',
+  //     }
+  //   }
+  // ).then((res) => {
+  //   setData(res.data);
+  // }).catch((err) => {
+
+  // });
+  // console.log('render');
+
+
+  useEffect(() => {
+    console.log('lio');
+  }, []);
+
 
   return (
-    <div className='p-7'>
-
-
-      <UserForm />
-      {/* <CardDisplay image={image} datas={[11, 22, 44, 55]} />
-      <CardDisplay image={image1} /> */}
-
-
-
+    <div>
 
 
 
