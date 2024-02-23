8d471c858a0208fa98a8d25ada16f24007404e69
Rabyn900 <82014570+Rabyn-Collab@users.noreply.github.com>
2024-02-23 09:16:32 +0545

    changes

---
+++ src/components/constants.js
@@ -0,0 +1,12 @@
+
+
+
+
+const baseUrl = 'https://api.themoviedb.org/3';
+export const imageUrl = 'https://image.tmdb.org/t/p/w500';
+
+export const getPopular = `${baseUrl}/movie/popular`;
+export const getTopRated = `${baseUrl}/movie/top_rated`;
+
+export const api_key = '92c1e33f015755d27a231793c44ecfed';
+
--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -2,57 +2,55 @@ import axios from 'axios'
 import React from 'react'
 import { useEffect } from 'react';
 import { useState } from 'react';
+import { api_key, getPopular, imageUrl } from '../components/constants';
 
 const HomePage = () => {
 
-  const [data, setData] = useState({});
+  const [data, setData] = useState(null);
+  const [load, setLoad] = useState(false);
+  const [err, setErr] = useState('');
+
 
 
   const fetchData = async () => {
     try {
-      const response = await axios.get('', {
+      setLoad(true);
+      const response = await axios.get(getPopular, {
         params: {
-          api_key: ''
+          api_key: api_key
         }
       });
-
+      setLoad(false);
+      setData(response.data.results);
     } catch (err) {
+      setLoad(false);
+      setErr(err.message);
 
     }
   }
 
-  console.log('heloo jeee');
-
-  // console.log('run1');
-  // console.log('run2');
-  // setTimeout(() => {
-  //   console.log('hello jee run4');
-  // }, 4000);
-  // console.log('run3')
-
-  // axios.get('https://api.themoviedb.org/3/movie/popular',
-  //   {
-  //     params: {
-  //       'api_key': '92c1e33f015755d27a231793c44ecfed',
-  //     }
-  //   }
-  // ).then((res) => {
-  //   setData(res.data);
-  // }).catch((err) => {
-
-  // });
-  // console.log('render');
 
 
   useEffect(() => {
-    console.log('lio');
+    fetchData();
   }, []);
 
-
+  if (load) {
+    return <h1>LoaDING....</h1>;
+  }
+  // if (err) {
+  //   return <h1>{err}</h1>
+  // }
+  console.log(data);
   return (
     <div>
 
-
+      {data && data.map((movie) => {
+        return <div key={movie.id}>
+          <h1>{movie.title}</h1>
+          <img src={`${imageUrl}${movie.poster_path}`} alt="" />
+        </div>
+      })}
 
 
     </div>
